d7a0e4c3f33912c259da156274f2043582dfb544
Matthew Gusmano <matthew.gusmano@gmail.com>
2016-08-16 20:39:20 -0400

    starting login page

---
+++ app/components/Main.js
@@ -0,0 +1,66 @@
+// Include React 
+var React = require('react');
+
+// Here we include all of the sub-components
+var login= require('./children/login');
+var homepage = require('./children/homepage');
+
+
+// Helper Function
+var helpers = require('./utils/helpers.js');
+
+// This is the main component. 
+var Main = React.createClass({
+
+	// Here we set a generic state associated with the number of clicks
+	getInitialState: function(){
+		return {
+
+		}
+	},	
+
+
+
+
+
+	// Here we render the function
+	render: function(){
+
+		return(
+
+			<div className="container">
+
+				<div className="row">
+
+					<div className="jumbotron">
+						<h2 className="text-center">Address Finder!</h2>
+						<p className="text-center"><em>Enter a landmark to search for its exact address (ex: "Eiffel Tower").</em></p>
+					</div>
+
+					<div className="col-md-6">
+					
+						<Form setTerm={this.setTerm}/>
+
+					</div>
+
+					<div className="col-md-6">
+				
+						<Results address={this.state.results} />
+
+					</div>
+
+				</div>
+
+				<div className="row">
+
+					<History history={this.state.history}/> 
+
+				</div>
+
+			</div>
+		)
+	}
+});
+
+// Export the component back for use in other files
+module.exports = Main;
