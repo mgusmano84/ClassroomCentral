41bda4bd67b07f7fc1fd936f5b655939bbc43be4
Matthew Gusmano <matthew.gusmano@gmail.com>
2016-08-31 21:17:23 -0400

    added messages schema to data folder

--- data/Schema.sql
+++ data/Schema.sql
@@ -8,3 +8,15 @@ create table if not exists Users(
    username varchar(100) unique,
    password varchar(100)
 )
+
+
+-- Message Feature
+
+CREATE TABLE   Messages (
+`msg_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+`message` varchar(200) NOT NULL,
+`user_id` int(11) NOT NULL,
+`likes` int(11) DEFAULT NULL,
+`created` int(11) DEFAULT NULL,
+FOREIGN KEY (user_id) REFERENCES Users(userId)
+);
