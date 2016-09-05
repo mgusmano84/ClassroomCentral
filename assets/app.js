473b09de67a26f0d489aa5a47a7cd7d8d76ebdfc
Matthew Gusmano <matthew.gusmano@gmail.com>
2016-09-04 22:58:11 -0400

    added comment post modal

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,22 @@
+    $(document).ready(function(){
+    	var app = {
+		currentURL: window.location.origin,
+		}
+
+
     $("#menu-toggle").click(function(e) {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
     });
+
+
+    $('#makeComment').on('click',function(){
+		$('#commentModal').modal('show');
+	return false;
+	});
+
+
+
+
+
+})
