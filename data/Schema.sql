0a3cf454987e8c59025923ff1152742fc4ac789d
Matthew Gusmano <matthew.gusmano@gmail.com>
2016-09-06 19:32:32 -0400

    added to schema

--- data/Schema.sql
+++ data/Schema.sql
@@ -1,7 +1,116 @@
+
+-- backup
+
 create database ClassroomCentral;
 
-
 use ClassroomCentral;
+
+create table if not exists Users(
+   userId integer primary key auto_increment,
+   username varchar(100) unique,
+   password varchar(100),
+   isTeacher BOOLEAN DEFAULT NULL,
+   email varchar(100)	
+)
+
+
+CREATE TABLE   Post (
+	msg_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	text varchar(200) NOT NULL,
+	userId int(11) NOT NULL,
+	created DATETIME DEFAULT NULL,
+	FOREIGN KEY (userId) REFERENCES Users(userId)
+);
+
+
+
+CREATE TABLE Comments (
+	text varchar(200) NOT NULL,
+	post_id int(11) NOT NULL,
+	created DATETIME DEFAULT NULL,
+	userId int(11) NOT NULL,
+	FOREIGN KEY (userId) REFERENCES Users(userId),
+	FOREIGN KEY (post_id) REFERENCES Post(msg_id)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- create database ClassroomCentral;
+
+
+-- use ClassroomCentral;
+
+
+
+-- create table if not exists Users(
+--    userId integer primary key auto_increment,
+--    username varchar(100) unique,
+--    password varchar(100),
+--    isTeacher BOOLEAN DEFAULT NULL,
+--    email varchar(100)	
+-- )
+
+-- --    create table Class (
+-- -- 	classId integer primary key auto_increment
+-- -- )
+
+-- -- Message Feature
+
+-- CREATE TABLE   Post (
+-- 	msg_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+-- 	text varchar(200) NOT NULL,
+-- 	userId int(11) NOT NULL,
+-- 	created int(11) DEFAULT NULL,
+-- 	-- classId int(11),
+-- 	-- FOREIGN KEY (classId) REFERENCES Class(classId),
+-- 	FOREIGN KEY (userId) REFERENCES Users(userId)
+-- );
+
+-- -- Comment Feature
+
+-- CREATE TABLE Comments (
+-- 	text varchar(200) NOT NULL,
+-- 	post_id int(11) NOT NULL,
+-- 	created int(11) DEFAULT NULL,
+-- 	userId int(11) NOT NULL,
+-- 	FOREIGN KEY (userId) REFERENCES Users(userId),
+-- 	FOREIGN KEY (post_id) REFERENCES Post(msg_id)
+-- );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- Save for later ********************
+
 
 -- User Feature
 
@@ -14,93 +123,6 @@ use ClassroomCentral;
 --    email varchar(100),	
 --    FOREIGN KEY (classId) REFERENCES Class(classId)
 --    );
-
-create table if not exists Users(
-   userId integer primary key auto_increment,
-   username varchar(100) unique,
-   password varchar(100),
-   isTeacher BOOLEAN DEFAULT NULL,
-   email varchar(100)	
-)
-
---    create table Class (
--- 	classId integer primary key auto_increment
--- )
-
--- Message Feature
-
-CREATE TABLE   Post (
-	msg_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	text varchar(200) NOT NULL,
-	userId int(11) NOT NULL,
-	created int(11) DEFAULT NULL,
-	-- classId int(11),
-	-- FOREIGN KEY (classId) REFERENCES Class(classId),
-	FOREIGN KEY (userId) REFERENCES Users(userId)
-);
-
--- Comment Feature
-
-CREATE TABLE Comments (
-	text varchar(200) NOT NULL,
-	post_id int(11) NOT NULL,
-	created int(11) DEFAULT NULL,
-	userId int(11) NOT NULL,
-	FOREIGN KEY (userId) REFERENCES Users(userId),
-	FOREIGN KEY (post_id) REFERENCES Post(msg_id)
-);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- backup
-
-
-drop database ClassroomCentral;
-
-
-create database ClassroomCentral;
-
-
-use ClassroomCentral;
-
-create table if not exists Users(
-   userId integer primary key auto_increment,
-   username varchar(100) unique,
-   password varchar(100),
-   isTeacher BOOLEAN DEFAULT NULL,
-   email varchar(100)	
-)
-
-
-CREATE TABLE   Post (
-	msg_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	text varchar(200) NOT NULL,
-	userId int(11) NOT NULL,
-	created DATETIME DEFAULT NULL,
-	FOREIGN KEY (userId) REFERENCES Users(userId)
-);
-
-
-
-CREATE TABLE Comments (
-	text varchar(200) NOT NULL,
-	post_id int(11) NOT NULL,
-	created DATETIME DEFAULT NULL,
-	userId int(11) NOT NULL,
-	FOREIGN KEY (userId) REFERENCES Users(userId),
-	FOREIGN KEY (post_id) REFERENCES Post(msg_id)
-);
 
 
 
